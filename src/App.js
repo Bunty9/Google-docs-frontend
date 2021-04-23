46cd9139f6f62394fab1100f81df4b499f1e8bfc
Bunty9 <cbipin2000@gmail.com>
2021-04-23 21:00:32 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import TextEditor from './TextEditor';
-
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route
+} from 'react-router-dom'
+import {v4 as uuidV4} from "uuid"
 
 function App() {
   return (
-    <div className="App">
-      <TextEditor/>
-      
-    </div>
+    <Router> 
+      <Switch> 
+        <Route path="/" exact>
+          <Redirect to= {`/documents/${uuidV4()}`}  /> 
+        </Route>
+        <Route path="/documents/:id" >
+        <TextEditor/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,9 +1,13 @@
 import React, { useCallback } from 'react'
-import {useEffect , useRef} from 'react'
+import {useEffect , useState} from 'react'
 import Quill from "quill"
 import "./quill.snow.css";
+import io from "socket.io-client"
+import { useParams } from "react-router-dom"
 
+const SAVE_INTERVAL_MS = 5000;
 const TOOLBAR_OPTIONS = [
+    ['omega'],
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
     [{ font: [] }],
     [{ list: "ordered" }, { list: "bullet" }],
@@ -14,19 +18,98 @@ const TOOLBAR_OPTIONS = [
     ["clean"],
 ]
 
+
+
 function TextEditor() {
+    const { id: documentId} = useParams()
+    const [socket,setSocket] = useState()
+    const [quill,setQuill] = useState() 
+
+    useEffect(() =>{
+        const s = io("http://localhost:3001")
+        setSocket(s)
+        return ()=>{
+            s.disconnect();
+        }
+    },[])
+
+
+    useEffect(() =>{
+        if (socket == null || quill == null) return;
+        socket.once('load-document', data =>{
+            quill.setContents(data)
+            quill.enable()
+        })
+        socket.emit('get-document',documentId)
+    },[socket,quill , documentId])
+
+    useEffect(() =>{
+        if (socket == null || quill == null) return;
+        const handler = function(delta, oldDelta, source) {
+                if (source !== 'user') {
+                return
+                }
+                socket.emit('send-changes',delta)
+        }
+        quill.on('text-change', handler)
+        return ()=>{
+            quill.off('text-change', handler )
+        }
+    },[socket,quill])
+
+    
+    useEffect(() =>{
+        if (socket == null || quill == null) return;
+        const handler = function(delta) {
+            quill.updateContents(delta)
+        }
+        socket.on('receive-changes', handler)
+        return ()=>{
+            socket.off('receive-changes', handler )
+        }
+    },[socket,quill])
+
+    
+    useEffect(() =>{
+        if (socket == null || quill == null) return;
+        const interval = setInterval(() =>{
+            socket.emit("save-document", quill.getContents())
+        },SAVE_INTERVAL_MS)
+        return ()=>{
+            clearInterval(interval)
+        }
+    },[socket,quill])
+
+
+
+
+
     const wrapperRef =  useCallback((wrapper) => {
         if (wrapper==null) {return;}
         wrapper.innerHTML = ""
         const editor = document.createElement('div')
         wrapper.append(editor)
-        new Quill(editor, {theme: "snow" , modules: {toolbar: TOOLBAR_OPTIONS}} )
+        const q = new Quill(editor, {theme: "snow" , modules: {toolbar: TOOLBAR_OPTIONS}} )
 
+        var customButton = document.querySelector('.ql-omega');
+        customButton.addEventListener('click', function() {
+            console.log("clicked")
+            window.location = "https://github.com/Bunty9";
+        });
+        q.enable(false)
+        q.setText("Loading . . . ")
+        setQuill(q)
     }, [])
     
+
+
+
     return (
-        <div className = "container" ref={wrapperRef}>        </div>
+            <div className = "container" ref={wrapperRef}>       
+            </div>
     )
 }
 
 export default TextEditor
+
+
